09856816ed66eeb32efe86b3821bef3d96b0d242
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-20 13:00:53 -0500

    Added tagging

--- gringotts.js
+++ gringotts.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 var http = require("request");
-var env = require("./env");
+var gh = require("./helpers/github");
 
 app.use(express.static("public"));
 
@@ -12,30 +12,19 @@ app.get("/", function(req, res){
 
 app.get("/:org.json", function(req, res){
   var org = req.params["org"];
-  http.get(gh_repos_for(org), function(err, response){
-    res.header("Content-Type", "application/json");
-    res.send(response.body);
-  });
+  var repos = [];
+  (function load_repos(page){
+    http.get(gh.repos_for(org, page), function(err, response){
+      var pages = gh.page_from(response.headers.link);
+      repos = repos.concat(JSON.parse(response.body));
+      if(pages["next"]){
+        console.log("Loading page " + pages["next"] + " of " + pages["last"] + "...");
+        load_repos(pages["next"]);
+      }else res.json(repos.map(gh.condense_repo));
+    });
+  }());
 });
 
 app.listen(3000, function(){
   console.log("Listening on 3000.");
 });
-
-function gh_repos_for(org, pageNum){
-  return {
-    "url": "https://api.github.com/orgs/" + org + "/repos",
-    "auth": {
-      "user": env.gh_username,
-      "pass": env.gh_access_token,
-      "sendImmediately": true
-    },
-    "qs": {
-      "per_page": 100,
-      "page": (pageNum || 1)
-    },
-    "headers": {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
-    }
-  }
-}
---
+++ helpers/github.js
@@ -0,0 +1,49 @@
+module.exports = (function(){
+  var gh = {};
+  var env = require("../env");
+  gh.repos_for = function(org, pageNum){
+    return {
+      "url": "https://api.github.com/orgs/" + org + "/repos",
+      "auth": {
+        "user": env.gh_username,
+        "pass": env.gh_access_token,
+        "sendImmediately": true
+      },
+      "qs": {
+        "per_page": 100,
+        "page": (pageNum || 1)
+      },
+      "headers": {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
+      }
+    }
+  }
+  gh.page_from = function(header){
+    var regexp = /(\&page=)([0-9])(>; rel=\")([a-z]+)/ig;
+    var out = {};
+    header.replace(regexp, function(){
+      out[arguments[4]] = arguments[2];
+    });
+    return out;
+  }
+  gh.parse_description = function(description){
+    var tagMatcher = /\[[^\]]*\]/ig, description = (description || "");
+    var tags = description.match(tagMatcher);
+    var english = description.replace(tagMatcher, "").trim();
+    if(tags) tags = tags[0].toLowerCase().replace(/[\[\]]/g, "").split(/,[ ]*/);
+    return { tags: tags, english: english }
+  }
+  gh.condense_repo = function(repo){
+    var description = gh.parse_description(repo.description);
+    return {
+      id: repo.id,
+      name: repo.name,
+      url: repo.html_url,
+      description: description.english,
+      tags: description.tags,
+      stars: repo.stargazers_count,
+      watchers: repo.watchers_count
+    }
+  }
+  return gh;
+}());
