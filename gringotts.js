24fba78c8b78bc4fd000ae050dae3fdaf787018e
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-22 12:25:11 -0500

    Added login

--- gringotts.js
+++ gringotts.js
@@ -17,6 +17,28 @@ app.use(express.static(__dirname + "/public"));
 app.use(cookies(env.secret));
 app.set("port", process.env.PORT || 3000);
 
+app.get("/login", function(req, res){
+  gh.login(res);
+});
+
+app.get("/callback", function(req, res){
+  request({
+    method: "POST",
+    uri: "https://github.com/login/oauth/access_token",
+    headers: {
+      "Accept": "application/json"
+    },
+    json: {
+      client_id: env.gh.id,
+      client_secret: env.gh.secret,
+      code: req.query.code
+    }
+  }, function(err, data){
+    res.cookie("gh_token", data.body.access_token);
+    res.redirect("/");
+  });
+});
+
 app.get("/*", function(req, res){
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
